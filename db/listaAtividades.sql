8d730fc608dd5ec2b37c966e7a75edc6020734ce
Ricardo Jeferson Hendges <ricardohe04@gmail.com>
2022-09-21 10:03:51 -0300

    db ajuste atividades

--- db/listaAtividades.sql
+++ db/listaAtividades.sql
@@ -1,29 +1,29 @@
-insert into atividade (id, titulo, descricao, entrada, saida) 
-               values (1, 'X do Guedes', 'Em 2012 foi alcançado um novo recorde mundial na famosa Competição de Cachorros-Quentes do Nathan: o campeão, Joey Chestnut, devorou 68 cachorros-quentes em dez minutos, um aumento incrível em relação aos 62 sanduíches devorados pelo mesmo Chestnut em 2011.
+insert into atividade (id, code, dificuldade, quantidade, titulo, descricao, entrada, saida)
+               values (1, 'A', 1, 11253, 'X do Guedes', 'Em 2012 foi alcançado um novo recorde mundial na famosa Competição de Cachorros-Quentes do Nathan: o campeão, Joey Chestnut, devorou 68 cachorros-quentes em dez minutos, um aumento incrível em relação aos 62 sanduíches devorados pelo mesmo Chestnut em 2011.
 O restaurante Nathan’s Famous Corporation, localizado no Brooklyn, NY, é o responsável pela competição. Eles produzem deliciosos cachorros-quentes, mundialmente famosos, mas quando o assunto é matemática eles não são tão bons. Eles desejam ser listados no Livro de Recordes do Guinness, mas para isso devem preencher um formulário descrevendo os fatos básicos da competição. Em particular, eles devem informar o número médio de cachorros-quentes consumidos pelos participantes durante a competição.
 Você pode ajudá-los? Eles prometeram pagá-lo com um dos seus saborosos cachorros-quentes. Dados o número total de cachorros-quentes consumidos e o número total de participantes na competição, você deve escrever um programa para determinar o número médio de cachorros-quentes consumidos pelos participantes.
 ', 'A entrada consiste de uma única linha que contém dois inteiros H e P (1 ≤ H, P ≤ 1000) indicando respectivamente o número total de cachorros-quentes consumidos e o número total de participantes na competição.',
-'Seu programa deve produzir uma única linha com um número racional representando o número médio de cachorros-quentes consumidos pelos participantes. O resultado deve ser escrito como um número racional com exatamente dois dígitos após o ponto decimal, arredondado se necessário.')
+'Seu programa deve produzir uma única linha com um número racional representando o número médio de cachorros-quentes consumidos pelos participantes. O resultado deve ser escrito como um número racional com exatamente dois dígitos após o ponto decimal, arredondado se necessário.');
 
-insert into atividade (id, titulo, descricao, entrada, saida) 
-               values (2, 'Fatorando', 'O fatorial de um número inteiro positivo N, denotado por N!, é definido como o produto dos inteiros positivos menores do que ou iguais a N. Por exemplo 4! = 4 × 3 × 2 × 1 = 24.
+insert into atividade (id, code, dificuldade, quantidade, titulo, descricao, entrada, saida)
+               values (2, 'B', 1, 2717, 'Fatorando', 'O fatorial de um número inteiro positivo N, denotado por N!, é definido como o produto dos inteiros positivos menores do que ou iguais a N. Por exemplo 4! = 4 × 3 × 2 × 1 = 24.
 Dado um inteiro positivo N, você deve escrever um programa para determinar o menor número k tal que N = a1! + a2! + ... + ak!, onde cada ai, para 1 ≤ i ≤ k, é um número inteiro positivo.
 Por exemplo, para N = 10 a resposta é 3, pois é possível escrever N como a soma de três números fatoriais: 10 = 3! + 2! + 2!. Para N = 25 a resposta é 2, pois é possível escrever N como a soma de dois números fatoriais: 25 = 4! + 1!.
-', 'A entrada consiste de uma única linha que contém um inteiro N (1 ≤ N ≤ 105).', 'Seu programa deve produzir uma única linha com um inteiro representando a menor quantidade de números fatoriais cuja soma é igual ao valor de N.')
+', 'A entrada consiste de uma única linha que contém um inteiro N (1 ≤ N ≤ 105).', 'Seu programa deve produzir uma única linha com um inteiro representando a menor quantidade de números fatoriais cuja soma é igual ao valor de N.');
 
-insert into atividade (id, titulo, descricao, entrada, saida) 
-               values (3, '', '', '', '')
-insert into atividade (id, titulo, descricao, entrada, saida) 
-               values (4, '', '', '', '')
-insert into atividade (id, titulo, descricao, entrada, saida) 
-               values (5, '', '', '', '')
-insert into atividade (id, titulo, descricao, entrada, saida) 
-               values (6, '', '', '', '')
-insert into atividade (id, titulo, descricao, entrada, saida) 
-               values (7, '', '', '', '')
-insert into atividade (id, titulo, descricao, entrada, saida) 
-               values (8, '', '', '', '')
-insert into atividade (id, titulo, descricao, entrada, saida) 
-               values (9, '', '', '', '')
-insert into atividade (id, titulo, descricao, entrada, saida) 
-               values (10, '', '', '', '')
+insert into atividade (id, code, dificuldade, quantidade, titulo, descricao, entrada, saida)
+               values (3, 'C', '', '', '', '');
+insert into atividade (id, code, dificuldade, quantidade, titulo, descricao, entrada, saida)
+               values (4, 'D', '', '', '', '');
+insert into atividade (id, code, dificuldade, quantidade, titulo, descricao, entrada, saida)
+               values (5, 'E', '', '', '', '');
+insert into atividade (id, code, dificuldade, quantidade, titulo, descricao, entrada, saida)
+               values (6, 'F', '', '', '', '');
+insert into atividade (id, code, dificuldade, quantidade, titulo, descricao, entrada, saida)
+               values (7, 'G', '', '', '', '');
+insert into atividade (id, code, dificuldade, quantidade, titulo, descricao, entrada, saida)
+               values (8, 'H', '', '', '', '');
+insert into atividade (id, code, dificuldade, quantidade, titulo, descricao, entrada, saida)
+               values (9, 'I', '', '', '', '');
+insert into atividade (id, code, dificuldade, quantidade, titulo, descricao, entrada, saida)
+               values (10, 'J', '', '', '', '');
