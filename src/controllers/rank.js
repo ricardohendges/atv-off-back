f1aa3c5440af50ff9cf076acb3278946e24f4409
Ricardo Jeferson Hendges <ricardohe04@gmail.com>
2022-09-21 16:22:58 -0300

    ajuste no rank

--- src/controllers/rank.js
+++ src/controllers/rank.js
@@ -14,12 +14,27 @@ const getRank = async (req, res) => {
                 let atv = {} 
                 atv.atv_id = b.atv_id
                 atv.atv_code = b.atv_code
-                atv.qtdeTentado = submissoes.submissao.filter(c => c.atv_id == b.atv_id && c.dup_id == a.dup_id).length
-                let correta = submissoes.submissao.filter(c => c.atv_id == b.atv_id && c.dup_id == a.dup_id && c.sub_status == 'Accepted').length
-                atv.status = correta > 0 ? 'Accepted' : (atv.qtdeTentado > 0) ? 'Tried' : null
+                let qtdeTentado = submissoes.submissao.filter(c => c.atv_id == b.atv_id && c.dup_id == a.dup_id).length
+                atv.qtdeTentado = qtdeTentado ? qtdeTentado : null
+                let correta = submissoes.submissao.find(c => c.atv_id == b.atv_id && c.dup_id == a.dup_id && c.sub_status == 'Accepted')
+                let incorretas = submissoes.submissao.filter(c => c.atv_id == b.atv_id && c.dup_id == a.dup_id && c.sub_status != 'Accepted')
+                atv.status = correta ? 'Accepted' : (atv.qtdeTentado > 0) ? 'Tried' : null
+                let inicio = new Date(process.env.INICIO_TORNEIO)
+                let dataFeita = new Date(correta?.sub_data)
+                dataFeita.setHours(dataFeita.getHours() - 3)
+                atv.pontos = Math.trunc(((dataFeita - inicio)/1000)/60*10) + (incorretas.length * 120)
                 return atv
             })
+            dupla.finalizadas = dupla.atividades.filter(d => d.status == 'Accepted').length
+            dupla.pontos = dupla.atividades.reduce((a,b) => (a.pontos||0) + (b.pontos||0))
             return dupla
+        })
+        rank.duplas.sort((a, b) => {
+            if (a.finalizadas > b.finalizadas) return -1
+            else if (a.finalizadas < b.finalizadas) return 1
+            if (a.pontos < b.pontos) return -1
+            else if (a.pontos > b.pontos) return 1
+            return 0
         })
         res.status(200).json(rank)
     } catch (err) {
