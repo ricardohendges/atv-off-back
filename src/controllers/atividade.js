55df44b5f6164ae107ecac35583f991f12128558
Ricardo Jeferson Hendges <ricardohe04@gmail.com>
2022-09-22 22:02:31 -0300

    ajuste atividade

--- src/controllers/atividade.js
+++ src/controllers/atividade.js
@@ -1,9 +1,24 @@
 const atividadesServices = require('../services/atividade')
+const submissaoServices = require('../services/submissao')
 
 const getAtividades = async (req, res) => {
-    atividadesServices.getAtividades()
-        .then(ret => res.status(200).json(ret))
-        .catch(err => res.status(500).json(err.message))
+    try {
+        const sub = await submissaoServices.getSubmissao(req.body, false)
+        const atv = await atividadesServices.getAtividades()
+        sub.submissao.sort((a,b) => {
+            if (a.sub_data < b.sub_data) return 1
+            else if (a.sub_data > b.sub_data) return -1
+            return 0
+        })
+        let atividades = atv.atividades.map(a => {
+            let atividade = a
+            atividade.atv_status = sub.submissao.find(b => b.atv_id==a.atv_id)?.sub_status
+            return atividade
+        })
+        res.status(200).json({atividades: atividades})
+    } catch(err) {
+        res.status(500).json(err.message)
+    }
 }
 
 module.exports.getAtividades = getAtividades
